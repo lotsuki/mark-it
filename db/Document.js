39d21fc9b08eb9d2c592154e87efc58d83516154
lotsuki <otsuki.lindsey@gmail.com>
2019-04-25 21:52:54 -0700

    post api working for same category/same subject

--- db/Document.js
+++ db/Document.js
@@ -3,17 +3,7 @@ const db = require('./index.js');
 
 const Schema = mongoose.Schema;
 
-// const documentSchema = new Schema ({
-//   category: String,
-//   subject: String,
-//   sites: [{
-//     title: String,
-//     url: String,
-//     date: String,
-//     starred: Boolean,
-//     favorites: Boolean
-//   }]
-// });
+
 
 const documentSchema = new Schema ({
   category: String,
