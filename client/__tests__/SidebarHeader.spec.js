4ea37a5241faa3945e3dd341e1998dbf06f0bf5a
lotsuki <otsuki.lindsey@gmail.com>
2019-05-17 13:23:47 -0700

    changed sidebarheader tests to check for text displayed

--- client/__tests__/SidebarHeader.spec.js
+++ client/__tests__/SidebarHeader.spec.js
@@ -5,17 +5,16 @@ import Quicklinks from '../components/Quicklinks';
 import {render, fireEvent, cleanup, waitForElement, waitForDomChange} from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
-
 describe('<SidebarHeader />', () => {
   afterEach(cleanup)
   it('displays text Quick links in Quicklinks component', () => {
-    const { getByTestId } = render(<Quicklinks />);
-     const sidebarHeaderElem = getByTestId('sidebarHeader');
-    expect(sidebarHeaderElem).toHaveTextContent('Quick links');
+    const { getByText } = render(<Quicklinks />);
+    const sidebarHeaderElem = getByText('Quick links');
+    expect(sidebarHeaderElem).toBeDefined();
   });
   it('displays text Bookmarks in Bookmarks component', () => {
-    const { getByTestId } = render(<Bookmarks />);
-    const sidebarHeaderElem = getByTestId('sidebarHeader');
-    expect(sidebarHeaderElem).toHaveTextContent('Bookmarks');
+    const { getByText } = render(<Bookmarks />);
+    const sidebarHeaderElem = getByText('My Bookmarks');
+    expect(sidebarHeaderElem).toBeDefined();
   });
 });
